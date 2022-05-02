e14ad4219c4ddcec3ff5d885428e52a8396cd086
Chris Hamill <chamill@ets.org>
2022-05-02 14:33:54 -0400

    fix type hint error

--- subreddit_comment_classification/utils/misc.py
+++ subreddit_comment_classification/utils/misc.py
@@ -10,6 +10,6 @@ def full_path(relative_path: str) -> Path:
     return Path(relative_path).resolve()
 
 
-def pretty_dumps(d: Dict[Any, Any]) -> None:
+def pretty_dumps(d: Dict[str, Any]) -> str:
     delimiter = '", "'
     return f'["{delimiter.join(sorted(d))}"]'
