89f5595a4e8b540bed6d7285eb8600ae45ad4efd
Chris Hamill <chamill@ets.org>
2022-05-02 14:43:57 -0400

    add feature extractors for generating word embeddings

---
+++ subreddit_comment_classification/embed/abstract.py
@@ -0,0 +1,56 @@
+"""
+Abstract base classes for word embeddings vectorizers to subclass.
+"""
+
+
+import dask.dataframe as dd
+import numpy as np
+
+from types import ModuleType
+
+
+class EmbeddingsVectorizer:
+
+    def __init__(self, **kwargs):
+        pass
+
+
+    def build_embeddings_matrix(self) -> None:
+        """Create matrix of pretrained word embeddings using a spacy model."""
+
+        num_tokens = self.vocabulary.size.compute()
+        embeddings_dim = self.nlp('The').vector.size
+        embeddings_matrix_shape = (num_tokens, embeddings_dim)
+
+        embeddings_matrix = np.zeros(embeddings_matrix_shape)
+        for i, token in enumerate(self.vocabulary):
+            embeddings_matrix[i] = self.nlp(token).vector
+
+        return embeddings_matrix
+
+
+    def fit_transform(self, preprocessed_data: dd.Series) -> np.ndarray:
+        """
+        Build up lexicon of unique tokens across all comments and use it to construct a
+        matrix of pretrained word embeddings using a spacy model.
+        """
+
+        self.get_vocabulary(preprocessed_data)
+        return self.build_embeddings_matrix()
+
+
+    def get_vocabulary(self, preprocessed_data: dd.Series) -> None:                   ### METHOD MAY NEED REFINEMENT
+        """
+        Build lexicon of all unique tokens across all passed preprocessed comments.
+
+        NOTE: THIS METHOD IS CURRENTLY VERY COARSE AND WILL NOT WORK UNLESS, AT A
+              MINIMUM, ALL PUNCTUATION MARKS HAVE BEEN REMOVED.
+        """
+
+        self.vocabulary = preprocessed_data.str.split().explode().unique()
+
+
+    def load_model(self, model: ModuleType) -> None:
+        """Load the spacy model to get embeddings from."""
+
+        self.nlp = model.load()
---
+++ subreddit_comment_classification/embed/embeddings.py
@@ -0,0 +1,71 @@
+"""
+Feature extractors for building up matrices of pretrained word embeddings using spacy
+models "en_core_web_lg" and "en_core_web_trf".
+"""
+
+
+import dask.dataframe as dd
+import numpy as np
+
+import en_core_web_lg
+import en_core_web_trf
+
+from overrides import overrides
+
+from embed.abstract import EmbeddingsVectorizer
+
+
+class LgEmbeddingsVectorizer(EmbeddingsVectorizer):
+
+    @overrides
+    def fit_transform(self, preprocessed_data: dd.Series) -> np.ndarray:
+        """
+        Use spacy's `en_core_web_lg` language model to construct a matrix of pretrained
+        word embeddings for the passed preprocessed comments.
+
+        Parameters
+        ----------
+        preprocessed_data : dd.Series
+            preprocessed comments raw text
+
+        Returns
+        -------
+        embedding_matrix : np.ndarray
+            matrix of pre-trained word embeddings for the passed preprocessed comments
+        """
+
+        self.load_model(en_core_web_lg)
+        embeddings_matrix = super().fit_transform(preprocessed_data)
+        return embeddings_matrix
+
+
+class TrfEmbeddingsVectorizer(EmbeddingsVectorizer):
+
+    @overrides
+    def fit_transform(self, preprocessed_data: dd.Series) -> np.ndarray:
+        """
+        Use spacy's `en_core_web_lg` language model to construct a matrix of pretrained
+        word embeddings for the passed preprocessed comments.
+
+        Parameters
+        ----------
+        preprocessed_data : dd.Series
+            preprocessed comments raw text
+
+        Returns
+        -------
+        embeddings_matrix : np.ndarray
+            matrix of pre-trained word embeddings for the passed preprocessed comments
+        """
+
+        # for more info on this error, see:
+        # https://github.com/explosion/spaCy/discussions/7643
+        err = ('The `en_core_web_trf` model works a bit differently from the '
+               '`en_core_web_lg` model. Crucially, it does not ship with pretrained '
+               'word embedding vectors. I need to look more into why this is and how '
+               'to get around it before `en_core_web_trf` will be available for use.')
+        raise NotImplementedError(err)
+
+        # self.load_model(en_core_web_trf)
+        # embeddings_matrix = super().fit_transform(preprocessed_data)
+        # return embeddings_matrix
