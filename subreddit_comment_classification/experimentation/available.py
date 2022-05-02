2ab96c56d7af8747b283109b37ca6dc50c6619d6
Chris Hamill <chamill@ets.org>
2022-05-02 14:52:53 -0400

    add module through which all models, extractors, processors, etc. are made available to the rest of the package

---
+++ subreddit_comment_classification/experimentation/available.py
@@ -0,0 +1,130 @@
+"""
+Load all objects available for use in this package.
+"""
+
+## ---- feature extractors
+from sklearn.feature_extraction.text import (
+    HashingVectorizer,
+    CountVectorizer,
+    TfidfTransformer,
+    TfidfVectorizer
+)
+from embed.embeddings import LgEmbeddingsVectorizer, TrfEmbeddingsVectorizer
+
+## ---- preprocessors
+from preprocessing.kitchen_sink import KitchenSinkPreprocessor
+from preprocessing.preprocessors import (
+    AccentRemover
+    ApostropheNormalizer
+    CaseNormalizer
+    CodeBlockRemover
+    HyperlinkRemover
+    InlineCodeRemover
+    NewlineCollapser
+    PunctuationRemover
+    QuoteRemover
+    SinglePreprocessor
+    Stemmer
+    WhitespaceNormalizer
+)
+
+
+## ---- sklearn models
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    AdaBoostRegressor,
+    GradientBoostingClassifier,
+    GradientBoostingRegressor,
+    RandomForestClassifier,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import (
+    BayesianRidge,
+    ElasticNet,
+    HuberRegressor,
+    Lars,
+    Lasso,
+    LinearRegression,
+    LogisticRegression,
+    RANSACRegressor,
+    Ridge,
+    RidgeClassifier,
+    SGDClassifier,
+    SGDRegressor,
+    TheilSenRegressor,
+)
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.svm import SVC, SVR, LinearSVC, LinearSVR
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+## ---- spacy models
+import en_core_web_lg
+import en_core_web_trf
+
+
+AVAILABLE = {
+    'FEATURE_EXTRACTORS' : {
+        'HashingVectorizer' : HashingVectorizer,
+        'CountVectorizer' : CountVectorizer,
+        'LgEmbeddingsVectorizer' : LgEmbeddingsVectorizer,
+        'TfidfTransformer' : TfidfTransformer,
+        'TfidfVectorizer' : TfidfVectorizer,
+        'TrfEmbeddingsVectorizer' : TrfEmbeddingsVectorizer
+    },
+    'PREPROCESSING' : {
+        'PREPROCESSORS' : {
+            'AccentRemover' : AccentRemover,
+            'ApostropheNormalizer': ApostropheNormalizer,
+            'CaseNormalizer': CaseNormalizer,
+            'CodeBlockRemover': CodeBlockRemover,
+            'HyperlinkRemover': HyperlinkRemover,
+            'InlineCodeRemover': InlineCodeRemover,
+            'NewlineCollapser': NewlineCollapser,
+            'PunctuationRemover': PunctuationRemover,
+            'QuoteRemover': QuoteRemover,
+            'SinglePreprocessor': SinglePreprocessor,
+            'Stemmer': Stemmer,
+            'WhitespaceNormalizer': WhitespaceNormalizer
+        },
+        'PIPELINES' : {
+            'KitchenSinkPreprocessor' : KitchenSinkPreprocessor
+        }
+    },
+    'SKLEARN_MODELS' : {
+        'AdaBoostClassifier': AdaBoostClassifier,
+        'AdaBoostRegressor': AdaBoostRegressor,
+        'BayesianRidge': BayesianRidge,
+        'DecisionTreeClassifier': DecisionTreeClassifier,
+        'DecisionTreeRegressor': DecisionTreeRegressor,
+        'ElasticNet': ElasticNet,
+        'GradientBoostingClassifier': GradientBoostingClassifier,
+        'GradientBoostingRegressor': GradientBoostingRegressor,
+        'HuberRegressor': HuberRegressor,
+        'KNeighborsClassifier': KNeighborsClassifier,
+        'KNeighborsRegressor': KNeighborsRegressor,
+        'Lars': Lars, 'Lasso': Lasso,
+        'LinearRegression': LinearRegression,
+        'LinearSVC': LinearSVC,
+        'LinearSVR': LinearSVR,
+        'LogisticRegression': LogisticRegression,
+        'MLPClassifier': MLPClassifier,
+        'MLPRegressor': MLPRegressor,
+        'MultinomialNB': MultinomialNB,
+        'RANSACRegressor': RANSACRegressor,
+        'RandomForestClassifier': RandomForestClassifier,
+        'RandomForestRegressor': RandomForestRegressor,
+        'Ridge': Ridge,
+        'RidgeClassifier': RidgeClassifier,
+        'SGDClassifier': SGDClassifier,
+        'SGDRegressor': SGDRegressor,
+        'SVC': SVC,
+        'SVR': SVR,
+        'TheilSenRegressor': TheilSenRegressor
+    },
+    'SPACY_MODELS' : {
+        'Lg' : lambda: en_core_web_lg.load(),
+        'Trf' : lambda: en_core_web_trf.load()
+    }
+}
