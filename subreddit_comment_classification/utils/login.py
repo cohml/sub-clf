88a9a06d81f2102bd97d7a86b8a6923c7d287e19
Chris Hamill <chamill@ets.org>
2022-05-03 09:28:17 -0400

    update module docstring

--- subreddit_comment_classification/utils/login.py
+++ subreddit_comment_classification/utils/login.py
@@ -1,5 +1,5 @@
 """
-Utility functions for easily connecting to the Reddit API.
+Object representing a Reddit session.
 """
 
 
@@ -16,6 +16,7 @@ class Reddit:
 
     def __init__(self):
         """Log into Reddit's backend API."""
+
         with open(CREDENTIALS_FILEPATH) as credentials_fh:
             self.credentials = json.load(credentials_fh)
             self.session = praw.Reddit(**self.credentials)
