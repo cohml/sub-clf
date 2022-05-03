cc769bc8d3a1c4e9a0ac414e18206fb67ec9a1f3
Chris Hamill <chamill@ets.org>
2022-05-03 09:36:04 -0400

    use pathlib API

--- subreddit_comment_classification/utils/login.py
+++ subreddit_comment_classification/utils/login.py
@@ -17,6 +17,6 @@ class Reddit:
     def __init__(self):
         """Log into Reddit's backend API."""
 
-        with open(CREDENTIALS_FILEPATH) as credentials_fh:
+        with CREDENTIALS_FILEPATH.open() as credentials_fh:
             self.credentials = json.load(credentials_fh)
             self.session = praw.Reddit(**self.credentials)
