b1ae900a9e34e1d5c7594886e06041000d9a21d4
Chris Hamill <chamill@ets.org>
2022-06-13 11:25:47 -0400

    shorten default parquet file names

--- sub_clf/util/defaults.py
+++ sub_clf/util/defaults.py
@@ -47,7 +47,7 @@ DEFAULTS = {
         },
         'TO_PARQUET_KWARGS' : {
             'compression' : 'gzip',
-            'name_function' : lambda i: f'{i+1:08}.parquet.gz',
+            'name_function' : lambda i: f'{i+1:06}.parquet.gz',
             'partition_on' : ['subreddit', 'post_id'],
             'schema' : _PARQUET_SCHEMA,
             'write_metadata_file' : False
